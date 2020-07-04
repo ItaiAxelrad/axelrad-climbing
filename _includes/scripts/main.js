b3c1fbb9390e28d600c616c9b4282c969abb84fa
ItaiAxelrad <itaiaxelrad@gmail.com>
2020-07-03 20:18:27 -0600

    mobile

--- _includes/scripts/main.js
+++ _includes/scripts/main.js
@@ -7,3 +7,17 @@ if ('serviceWorker' in navigator) {
       .catch((err) => console.log(`Service Worker: ${err}`)); // Registration error
   });
 }
+
+const body = document.querySelector('body')
+const toggleBtn = document.querySelector('button')
+// toggle button event listener
+toggleBtn.addEventListener('click', () => {
+    // check the user's preferred color scheme
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        body.classList.toggle('scheme-light')
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        body.classList.toggle('scheme-dark')
+    } else if (window.matchMedia('(prefers-color-scheme: no-preference)').matches) {
+        body.classList.toggle('scheme-dark')
+    }
+})
