acb0b553223037b836a712e0a71f67b5715c58a9
ItaiAxelrad <itaiaxelrad@gmail.com>
2020-08-09 17:02:06 -0600

    fix CMS

--- src/includes/scripts/main.js
+++ src/includes/scripts/main.js
@@ -11,20 +11,35 @@ if ('serviceWorker' in navigator) {
 const body = document.querySelector('body')
 const toggleBtn = document.querySelector('#schemeBtn')
 const useLink = document.querySelector('#schemeBtn use')
+
 // set initial preferences
-if (window.matchMedia('(prefers-color-scheme: dark)').matches || localStorage.getItem('mode') == 'dark' ) {
+if (localStorage.getItem('mode') == null) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         body.classList.add('scheme-dark')
         body.classList.remove('scheme-light')
         useLink.setAttribute("xlink:href", "/images/defs.svg#light")
         localStorage.setItem('mode', 'dark')
-    } else if (window.matchMedia('(prefers-color-scheme: light)').matches || localStorage.getItem('mode') == 'light') {
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
         body.classList.add('scheme-light')
         body.classList.remove('scheme-dark')
         useLink.setAttribute("xlink:href", "/images/defs.svg#dark")
         localStorage.setItem('mode', 'light')
     }
-// toggle button event listener
-toggleBtn.addEventListener('click', () => {
+} else {
+    if (localStorage.getItem('mode') == 'dark' ) {
+        body.classList.add('scheme-dark')
+        body.classList.remove('scheme-light')
+        useLink.setAttribute("xlink:href", "/images/defs.svg#light")
+        localStorage.setItem('mode', 'dark')
+    } else if (localStorage.getItem('mode') == 'light') {
+        body.classList.add('scheme-light')
+        body.classList.remove('scheme-dark')
+        useLink.setAttribute("xlink:href", "/images/defs.svg#dark")
+        localStorage.setItem('mode', 'light')
+    }
+}
+
+const toggleMode = async() => {
     // check the user's preferred color scheme
     if (localStorage.getItem('mode') == 'dark') {
         body.classList.remove('scheme-dark')
@@ -37,4 +52,7 @@ toggleBtn.addEventListener('click', () => {
         useLink.setAttribute("xlink:href", "/images/defs.svg#light")
         localStorage.setItem('mode', 'dark')
     } 
-})
+}
+
+// toggle button event listener
+toggleBtn.addEventListener('click', toggleMode )
--- src/posts/2009-02-14-DeadPointMagazineFeature/index.md
+++ src/posts/2009-02-14-DeadPointMagazineFeature/index.md
@@ -16,8 +16,8 @@ Here is a link to Climbing Narc's write up: <https://climbingnarc.com/2009/02/is
 
 Here are a few highlights from issue 3:
 
-- Video of [Joe Kinder](http://joekindkid.com/) sending Souther Comfort Right (5.14b) at Little River Canyon, AL
+- Video of [Joe Kinder](http://joekindkid.com/) sending Southern Comfort Right (5.14b) at Little River Canyon, AL
 - An interview with twins Itai and Eden Axelrad
 - A timely profile of Julian Bautista, who just took 5th place at the [Adult ABS Nationals](https://climbingnarc.com/2009/02/abs-10-national-bouldering-championships-results) comp despite having food poisoning
 - Interesting thoughts from Jason Kehl on the process one goes through when doing the FA of a highball boulder problem
-- A jealousy inducing story about the untapped hard route potential in Catalunya, Spain*
+- A jealousy inducing story about the untapped hard route potential in Catalunya, Spain
