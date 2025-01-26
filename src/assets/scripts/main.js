84a043cc46e8bf76f96fa9a8b0771558cc369685
ItaiAxelrad <itaiaxelrad@gmail.com>
2025-01-26 11:36:42 -0700

    refactor: remove unused scripts

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,12 +1,3 @@
-// register a service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js') // register a service worker
-      .then((reg) => console.log('Service Worker: Registered')) // Registration was successful
-      .catch((err) => console.log(`Service Worker: ${err}`)); // Registration error
-  });
-}
 // mobile menu toggle
 const menuBtn = document.querySelector('#menuBtn');
 const menuSvg = document.querySelector('#menuSvg use');
